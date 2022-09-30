db491a7d472ec60bbc67d9853fa68414dec6975d
shangopi <gopinath.19@cse.mrt.ac.lk>
2022-10-01 00:34:19 +0530

    aap.js change

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,12 @@
-import './App.css';
-import AreaChartView from './pages/ChartPages/AreaCustomize';
-import BubbleChartView from './pages/ChartPages/BubbleCustomize';
-import BarChartView from './pages/ChartPages/BarCustomize';
-import ArcChartView from './pages/ChartPages/Arc_Customize';
-import ChordChartView from './pages/ChartPages/Chord_Customize';
-import PieChartView from './pages/ChartPages/PieCustomize';
-import PolarAreaChartView from './pages/ChartPages/PolarAreaCustomization';
-import RadarChartView from './pages/ChartPages/RadarCustomize';
-import ScatterChartView from './pages/ChartPages/ScatterCustomize';
-import LineChartView from './pages/ChartPages/LineCustomize';
-const App =() => {
+import React from 'react'
+import Home from './pages/Home/Home';
+import ChartType from './pages/ChartType/ChartType';
+import ShowData from './pages/ShowData/ShowData';
+
+function App() {
   return (
-    <div className="App">
-      <ChordChartView/>
-      <br></br>
-      <hr />   
-      <ArcChartView/>
-      <br></br>
-      <hr />   
-      <BarChartView/>
-      <br></br>
-      <hr />   
-      <AreaChartView/> 
-      <br></br>
-      <hr />    
-      <BubbleChartView/>      
-      <br></br>
-      <hr />
-
-      <PieChartView/>       
-      <br></br>
-      <hr />
-      <PolarAreaChartView/>
-      <br></br>
-      <hr />
-
-      <RadarChartView/>
-      <br></br>
-      <hr />
-      
-
-      <ScatterChartView/>
-      <br></br>
-      <hr />
-
-      <LineChartView/>
-      <br></br>
-      <hr />
+    <div >
+      <ShowData />
     </div>
   );
 }
