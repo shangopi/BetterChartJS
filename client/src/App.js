4baef13d27a0e44323faef4a991490d5325795ac
Vinuhans <vinuhans123@gmail.com>
2022-11-20 11:18:29 +0530

    vinul

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,6 @@ import { Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ChartType from "./pages/ChartType/ChartType";
 import ShowData from "./pages/ShowData/ShowData";
-<<<<<<< HEAD
-import Navbar from "./components/Navbar/Navbar";
-import SavedCharts from "./pages/SavedCharts/SavedCharts";
-import Login from "./pages/Login/login";
-import Register from "./pages/Register/register";
-
-// import Arc_Customize from "./pages/ChartPages/Arc_Customize";
-// import AreaCustomize from "./pages/ChartPages/AreaCustomize";
-//import BarCustomize from "./pages/ChartPages/BarCustomize";
-// import BubbleCustomize from "./pages/ChartPages/BubbleCustomize";
-// import Chord_Customize from "./pages/ChartPages/Chord_Customize";
-// import LineCustomize from "./pages/ChartPages/LineCustomize";
-// import PieCustomize from "./pages/ChartPages/PieCustomize";
-// import PolarAreaCustomization from "./pages/ChartPages/PolarAreaCustomization";
-// import RadarCustomize from "./pages/ChartPages/RadarCustomize";
-// import ScatterCustomize from "./pages/ChartPages/ScatterCustomize";
-=======
 import BeginnerBarChart from "./pages/BeginnerPages/BeginnerBarChart";
 import BeginnerAreaChart from "./pages/BeginnerPages/BeginnerAreaChart";
 import BeginnerBubbleChart from "./pages/BeginnerPages/BeginnerBubbleChart";
@@ -36,7 +19,6 @@ import BeginnerChordChart from "./pages/BeginnerPages/BeginnerChordChart";
 import BeginnerSankeyChart from "./pages/BeginnerPages/BeginnerSankeyChart";
 import Register from "./pages/Register/register"
 import Login from "./pages/Login/login";
->>>>>>> 6eed330b83abe5f7bf85cebc3ee305923350df39
 
 function App() {
   return (
@@ -46,23 +28,6 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="charttype" element={<ChartType />} />
-<<<<<<< HEAD
-        <Route path="ShowData" element={<ShowData/>}/>
-        <Route path="savedCharts" element={<SavedCharts/>}/>
-        <Route path="chart" element={<Navbar />}>
-       
-          {/* <Route path="arc_chart" element={<Arc_Customize />} /> */}
-          {/* <Route path="area_chart" element={<AreaCustomize />} /> */}
-          {/* <Route path="bar_chart" element={<BarCustomize />} /> */}
-          {/* <Route path="bubble_chart" element={<BubbleCustomize />} /> */}
-          {/* <Route path="chord_chart" element={<Chord_Customize />} />
-          <Route path="line_chart" element={<LineCustomize />} />
-          <Route path="pie_chart" element={<PieCustomize />} />
-          <Route path="polar_chart" element={<PolarAreaCustomization />} />
-          <Route path="radar_chart" element={<RadarCustomize />} />
-          <Route path="scatter_chart" element={<ScatterCustomize />} /> */}
-        </Route>
-=======
         <Route path="showdata" element={<ShowData />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
@@ -77,7 +42,6 @@ function App() {
         <Route path="arcchart" element={<BeginnerArcChart />} />
         <Route path="chordchart" element={<BeginnerChordChart />} />
         <Route path="sankeychart" element={<BeginnerSankeyChart />} />
->>>>>>> 6eed330b83abe5f7bf85cebc3ee305923350df39
       </Routes>
     </BrowserRouter>
   );
