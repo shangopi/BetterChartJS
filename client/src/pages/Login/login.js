d308c01dcd75f1b98671def0c59a802935de2a5f
shangopi <gopinath.19@cse.mrt.ac.lk>
2022-11-22 22:16:19 +0530

    login & sign up pages ui. ux change

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -3,14 +3,25 @@ import Nav from "../../components/Navbar/Nav";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { TabTitle } from "../../utils/GeneralFunctions";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [iswrong, setiswrong] = useState(false);
+  const [isok, setisok] = useState(false);
+  const [loading,setloading] = useState(false);
+
 
   async function loginUser(event) {
     event.preventDefault();
+    setloading(true);
+    setisok(false);
+      setiswrong(false);
     const response = await fetch("http://localhost:4001/api/loginUser/login", {
       method: "POST",
       headers: {
@@ -27,10 +38,15 @@ function Login() {
     if (data.user) {
       console.log("This is dataaaa", data);
       localStorage.setItem("token", data.user);
-      alert("Login Successful");
+      setisok(true);
+      setloading(false);
+      setiswrong(false);
+      
       window.location.href = "/";
     } else {
-      alert("Please check the email and the password");
+      setisok(false);
+      setloading(false);
+      setiswrong(true);
     }
     console.log(data);
   }
@@ -38,13 +54,25 @@ function Login() {
 
   return (
     <div>
+      
       <Nav />
+      <Container>
       <div className="m-5">
         <h1>Login</h1>
         <hr />
         <br />
-        <br />{" "}
-        <div className="container col-6 offset-1">
+        <br />
+        <Row>
+          <Col md={6}>
+          <img  className="img-fluid" 
+     src={"https://cdni.iconscout.com/illustration/premium/thumb/graph-analysis-4090812-3391023.png"     } 
+     alt="logo"/>
+          </Col>
+        <Col  md={6}>
+        <div className=" offset-1">
+          <br></br>
+          <br></br>
+          <br></br>
           <Form onSubmit={loginUser}>
             <Form.Group class="mb-3">
               <Form.Label>Email</Form.Label>
@@ -69,8 +97,22 @@ function Login() {
               Login
             </Button>
           </Form>
+          <br></br>
+          <br></br>
+          {iswrong && <Alert className="lead px-3" variant="danger" >
+              Your Login is unsuccessful .. Please check the email and the password 
+        </Alert>}
+        {isok && <Alert className="lead px-3" variant="success" >
+               Your Login is Successful.. 
+        </Alert> }
+        {loading && <Alert className="lead px-3" variant="warning" >
+               Loading ... Please wait.. 
+        </Alert> }
         </div>
+        </Col>
+        </Row>
       </div>
+      </Container>
     </div>
   );
 }
